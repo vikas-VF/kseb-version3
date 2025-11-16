370b3ed8a75c19433c2168eabf0242291f907ee0
Vikas-VF <141120853+vikas-VF@users.noreply.github.com>
2025-11-16 12:12:16 +0530

    Delete dash directory

--- dash/callbacks/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Callbacks module"""
-from . import project_callbacks, forecast_callbacks, profile_callbacks, pypsa_callbacks, settings_callbacks
-__all__ = ['project_callbacks', 'forecast_callbacks', 'profile_callbacks', 'pypsa_callbacks', 'settings_callbacks']
